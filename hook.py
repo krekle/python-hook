4f2f403e425d5c0be102d4c81936d67c45cf44c2
krekle <kristian.ekle@gmail.com>
2015-03-24 21:33:53 +0100

    Added debug stdouts

--- hook.py
+++ hook.py
@@ -16,11 +16,15 @@ class hooks:
         print
         # send payload to delegator
         delegator = Delegator(payload)
+        print 'DELEGATOR PARSED'
         if delegator.action:
+            print 'REPO FOUND'
             man = Manager(delegator.action)
+            print 'RUNNING CMDS'
             man.do()
 
 
 if __name__ == '__main__':
+    print 'running'
     app = web.application(urls, globals())
     app.run()
